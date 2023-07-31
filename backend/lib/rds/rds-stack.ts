276c6f071a41b6dba70c35c1ae26f407306b4174
sp096 <sp096@hdm-stuttgart.de>
2023-07-31 10:28:30 +0200

    excluded ./dist

--- backend/lib/rds/rds-stack.ts
+++ backend/lib/rds/rds-stack.ts
@@ -7,10 +7,10 @@ import * as secrets from 'aws-cdk-lib/aws-secretsmanager';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as nodejs from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as cr from 'aws-cdk-lib/custom-resources';
-import { ICdkTsApiGatewayStackProps } from '../../bin/stack-config-types';
+import { IApiGatewayStackProps } from '../../bin/stack-config-types';
 
 export class RdsDatabase extends cdk.Stack {
-  constructor(scope: Construct, id: string, props: ICdkTsApiGatewayStackProps) {
+  constructor(scope: Construct, id: string, props: IApiGatewayStackProps) {
     super(scope, id, props);
 
     // VPC for RDS and Lambda resolvers
