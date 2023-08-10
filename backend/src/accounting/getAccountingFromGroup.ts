f2597377429c91c688b377109b04f7dcb87788b6
sp096 <sp096@hdm-stuttgart.de>
2023-08-10 18:36:36 +0200

    fixed getAccountingFromGroup

--- backend/src/accounting/getAccountingFromGroup.ts
+++ backend/src/accounting/getAccountingFromGroup.ts
@@ -1,55 +1,43 @@
 import { Handler } from "aws-lambda";
-import { In } from "typeorm";
 import { instantiateRdsClient } from "../utils/db-connection";
 import { Accounting } from "../models/accounting"; // Stellen Sie sicher, dass Sie den richtigen Pfad zum Accounting-Modell angeben
+import { createResponse } from "../utils/response-utils";
 
-export const getAccountingFromGroup: Handler = async (event: any) => {
+export const handler: Handler = async (event: any) => {
   let dataSource;
+  //path /Accounting/groupid
 
   try {
+    console.log("getAccountingFromGroup lambda starts here");
+
     dataSource = await instantiateRdsClient();
     const accountingRepository = dataSource.getRepository(Accounting);
 
     // Überprüfen, ob der "groupId" Parameter in der Event-Payload vorhanden ist
-    const groupId = event?.groupId;
+    const groupId = event.pathParameters.groupId;
     if (!groupId) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({
-          message: 'Ungültige Anforderungsdaten. "groupId" fehlt.',
-        }),
-      };
+      return createResponse(400, "groupId ist erforderlich.");
     }
+
+    console
 
     // Suchen aller Accounting-Datensätze in der Datenbank, die zur gegebenen groupId gehören
     const accountingFromGroup = await accountingRepository.find({
       where: { groupId },
     });
     if (accountingFromGroup.length === 0) {
-      return {
-        statusCode: 404,
-        body: JSON.stringify({
-          message:
-            "Es wurden keine Accounting-Datensätze für diese Gruppe gefunden.",
-        }),
-      };
+      return createResponse(404, "Keine Accounting-Datensätze gefunden.");
     }
+    
+    return createResponse(200, accountingFromGroup);
 
-    // Schließen Sie die Verbindung, wenn Sie fertig sind
-    await dataSource.destroy();
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "Accounting-Datensätze für die Gruppe gefunden.",
-        accounting: accountingFromGroup,
-      }),
-    };
   } catch (error) {
-    console.error(
-      "Fehler beim Abrufen der Accounting-Datensätze für die Gruppe:",
-      error
-    );
-    throw error;
+    console.error("Fehler beim Abrufen der Accounting-Datensätze für die Gruppe:",error);
+    return createResponse(500, "Kann Accounting-Datensätze nicht abrufen.");
+  }finally{
+    if(dataSource){
+      await dataSource.destroy();
+      console.log('Database connection closed.')
+    }
   }
 };
