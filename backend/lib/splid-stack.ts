07fa5761cfc43804a3fe7d08843f94363923ea02
sp096 <sp096@hdm-stuttgart.de>
2023-08-11 20:22:50 +0200

    updated createTransactions

--- backend/lib/splid-stack.ts
+++ backend/lib/splid-stack.ts
@@ -274,6 +274,14 @@ export class MyAppStack extends cdk.Stack {
     const getSettlingDebtsTransactionsResolver = createResolver('getSettlingDebtsTransactionsResolver', 'src/accounting/getSettlingDebtsTransactions.ts');
     getSettlingDebtsTransactionsResolver.node.addDependency(rdsInstance);
 
+    //transaction resolvers
+
+    const getTransactionByIdResolver = createResolver( 'getTransactionByIdResolver', 'src/transactions/getTransactionById.ts');
+    getTransactionByIdResolver.node.addDependency(rdsInstance);
+
+    const createTransactionResolver = createResolver( 'createTransactionResolver', 'src/transactions/createTransactions.ts');
+    createTransactionResolver.node.addDependency(rdsInstance);
+
     //group Integrations
 
     const getGroupIntegration = new apigateway.LambdaIntegration(
@@ -309,6 +317,11 @@ export class MyAppStack extends cdk.Stack {
 
     const getAccountingFromGroupIntegration = new apigateway.LambdaIntegration(getAccountingFromGroupResolver);
     const getSettlingDebtsTransactionsIntegration = new apigateway.LambdaIntegration(getSettlingDebtsTransactionsResolver);
+
+    //transaction Integrations
+
+    const getTransactionByIdIntegration = new apigateway.LambdaIntegration(getTransactionByIdResolver);
+    const createTransactionIntegration = new apigateway.LambdaIntegration(createTransactionResolver);
 
 
     // API Gateway RestApi
@@ -388,6 +401,11 @@ export class MyAppStack extends cdk.Stack {
 
     const accountingGroupIdSettledebtsResource = accountingGroupIdResource.addResource('settle-debts');
 
+    //transaction ressources
+
+    const transactionResource = secureResource.addResource('Transactions');
+    const transactionIdResource = transactionResource.addResource('{transactionId}');
+
     //group methods
     groupResource.addMethod('GET', getGroupIntegration, {
       requestModels: { 'application/json': model },
@@ -459,8 +477,18 @@ export class MyAppStack extends cdk.Stack {
       requestModels: { 'application/json': model },
       apiKeyRequired: true
     });
+    
+    //transaction methods
 
-    ///api/Groups/{groupId} /details
+    transactionIdResource.addMethod('GET', getTransactionByIdIntegration, {
+      requestModels: { 'application/json': model },
+      apiKeyRequired: true
+    });
+
+    transactionResource.addMethod('POST', createTransactionIntegration, {
+      requestModels: { 'application/json': model },
+      apiKeyRequired: true
+    });
 
     // API Usageplan
     const usageplan = api.addUsagePlan("UsagePlan", {
--- backend/src/transactions/createTransactions.ts
+++ backend/src/transactions/createTransactions.ts
@@ -2,9 +2,17 @@ import { Handler } from 'aws-lambda';
 import { instantiateRdsClient } from '../utils/db-connection';
 import { Transaction } from '../models/transaction';
 import { createResponse } from '../utils/response-utils';
+import { v4 as uuidv4 } from 'uuid';
 
 export const handler: Handler = async (event: any) => {
   let dataSource;
+    // {
+  //   "description": "string",
+  //   "senderUsername": "string",
+  //   "receiverUsername": "string",
+  //   "amount": 0,
+  //   "groupId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+  // }
 
   try {
     console.log('createTransactions lambda starts here')
@@ -15,10 +23,25 @@ export const handler: Handler = async (event: any) => {
 
     const inputTransactions: Transaction[] = JSON.parse(event.body);
 
-    const createdTransactions = await transactionRepository.save(inputTransactions);
+    if (Array.isArray(inputTransactions)) { //more than one transaction
+      inputTransactions.forEach(transaction => {
+        transaction.createdAt = new Date(Date.now());
+        transaction.id = uuidv4(),
+        console.log(transaction);
+      });
 
-    return createResponse(200, createdTransactions);
+      const createdTransactions = await transactionRepository.save(inputTransactions);
+      return createResponse(200, createdTransactions);
 
+    }else{ //only one transaction
+      const inputTransaction: Transaction = JSON.parse(event.body);
+      inputTransaction.createdAt = new Date(Date.now());
+      inputTransaction.id = uuidv4(),
+      console.log(inputTransaction);
+
+      const createdTransactions = await transactionRepository.save(inputTransaction);
+      return createResponse(200, createdTransactions);
+    }
   } catch (error) {
     console.error('Error creating transactions:', error);
     return createResponse(500, 'Cannot create Transactions.');
