1faa35395b5f0ddf7314ece968860a83f5fa1c59
sp096 <sp096@hdm-stuttgart.de>
2023-09-09 16:45:14 +0200

    comments and cleancode

--- backend/lib/rds/rds-construct.ts
+++ backend/lib/rds/rds-construct.ts
@@ -5,12 +5,27 @@ import * as iam from 'aws-cdk-lib/aws-iam';
 import * as cdk from "aws-cdk-lib";
 import * as secrets from "aws-cdk-lib/aws-secretsmanager";
 
+/**
+ * Properties required to initialize the RdsConstruct.
+ * 
+ * @property vpc - The VPC where the RDS instance will reside.
+ * @property securityGroup - The security group to associate with the RDS instance.
+ * @property role - The IAM role that will be granted read permissions to the RDS credentials.
+ */
 export interface RdsConstructProps {
   vpc: ec2.IVpc;
   securityGroup: ec2.ISecurityGroup;
   role: iam.IRole;
 }
 
+/**
+ * RdsConstruct creates a PostgreSQL RDS instance with associated credentials.
+ * 
+ * The construct:
+ * - Generates a secret for the RDS credentials.
+ * - Initializes a PostgreSQL RDS instance with the provided properties and configurations.
+ * - Grants the specified IAM role read access to the RDS credentials.
+ */
 export class RdsConstruct extends Construct {
   public readonly rdsInstance: rds.DatabaseInstance;
   public readonly credentials: secrets.ISecret;
@@ -18,6 +33,7 @@ export class RdsConstruct extends Construct {
   constructor(scope: Construct, id: string, props: RdsConstructProps) {
     super(scope, id);
 
+    // Generate a secret for the RDS credentials
     this.credentials = new secrets.Secret(this, 'DbCredentials', {
       generateSecretString: {
         secretStringTemplate: JSON.stringify({ username: 'splidUser' }),
@@ -27,7 +43,7 @@ export class RdsConstruct extends Construct {
       },
     });
     
-
+    // Create a PostgreSQL RDS instance using the provided properties and specific configurations.
     this.rdsInstance = new rds.DatabaseInstance(this, "PostgresRds", {
       vpc: props.vpc,
       securityGroups: [props.securityGroup],
@@ -50,6 +66,7 @@ export class RdsConstruct extends Construct {
       publiclyAccessible: true,
     });
 
+    // Grant the provided IAM role read access to the RDS credentials.
     this.rdsInstance.secret?.grantRead(props.role);
     this.credentials.grantRead(props.role);
   }
