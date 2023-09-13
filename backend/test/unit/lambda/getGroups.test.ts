a6585da3bd8fd52531bc2f51c0b570bc9235e156
sp096 <sp096@hdm-stuttgart.de>
2023-09-13 13:22:13 +0200

    added getGroups.test.ts

---
+++ backend/test/unit/lambda/getGroups.test.ts
@@ -0,0 +1,58 @@
+import { ApiGateway } from 'aws-cdk-lib/aws-events-targets';
+import { handler } from '../../../src/groups/getGroups';
+import { instantiateRdsClient } from '../../../src/utils/db-connection';
+import { createResponse } from '../../../src/utils/response-utils';
+import { APIGatewayProxyResult } from 'aws-lambda';
+
+jest.mock('../../../src/utils/db-connection');
+jest.mock('../../../src/utils/response-utils');
+
+describe('getGroups handler', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  test('should return groups successfully', async () => {
+    const mockGroups = [{ id: 1, name: 'Test Group' }];
+
+    const mockRepository = {
+      find: jest.fn().mockResolvedValue(mockGroups)
+    };
+
+    (instantiateRdsClient as jest.Mock).mockResolvedValue({
+      getRepository: jest.fn().mockReturnValue(mockRepository),
+      destroy: jest.fn()
+    });
+
+    (createResponse as jest.Mock).mockReturnValue({
+      statusCode: 200,
+      body: JSON.stringify(mockGroups)
+    });
+
+    const mockCallback = jest.fn();
+
+    const result = await handler({} as any, {} as any, mockCallback) as APIGatewayProxyResult;
+
+    expect(result.statusCode).toBe(200);
+    expect(JSON.parse(result.body)).toEqual([{ id: 1, name: "Test Group" }]);
+    
+  });
+
+  test('should handle errors when getting groups', async () => {
+    (instantiateRdsClient as jest.Mock).mockRejectedValue(new Error('DB Connection Error'));
+
+    (createResponse as jest.Mock).mockReturnValue({
+      statusCode: 500,
+      body: 'Error getting groups.'
+    });
+
+    const mockCallback = jest.fn();
+
+    const result = await handler({} as any, {} as any, mockCallback) as APIGatewayProxyResult;
+
+    expect(result.statusCode).toBe(500);
+    expect(result.body).toBe('Error getting groups.');
+});
+
+
+});
